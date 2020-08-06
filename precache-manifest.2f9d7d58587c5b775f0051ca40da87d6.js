74981968d3f981abceb40e5b318066e46bf2a1f5
Md Rasel Ahmed <47408774+raasel@users.noreply.github.com>
2020-08-07 01:23:15 +0600

    Add files via upload

---
+++ precache-manifest.2f9d7d58587c5b775f0051ca40da87d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b552903af2e6c92ac2fda4f48302a94",
+    "url": "/testapi/index.html"
+  },
+  {
+    "revision": "2e5bad418d9cd705f7fa",
+    "url": "/testapi/static/css/main.0a315abb.chunk.css"
+  },
+  {
+    "revision": "5043fde00459f16c0583",
+    "url": "/testapi/static/js/2.d31eca1c.chunk.js"
+  },
+  {
+    "revision": "80670e6d2a9be228a467582c01960795",
+    "url": "/testapi/static/js/2.d31eca1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5bad418d9cd705f7fa",
+    "url": "/testapi/static/js/main.69cd766d.chunk.js"
+  },
+  {
+    "revision": "36009eb903a3f6eb6e7d",
+    "url": "/testapi/static/js/runtime-main.11f7ae4a.js"
+  }
+]);
